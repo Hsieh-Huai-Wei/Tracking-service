65d6f98ac87490bcc12848d1cc4ab79214b2cabb
Raymond Hsieh <raymond.hw.hsieh@myviewboard.com>
2023-12-22 23:09:22 +0800

    add identify code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
+let identifyCode: string = '';
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get('healthcheck')
-  healthCheck(): string {
-    return 'ok';
+  healthCheck(): any {
+    if (identifyCode === '') {
+      identifyCode = uuidv4();
+    }
+    return {
+      health: 'ok',
+      code: identifyCode,
+    };
   }
 }
